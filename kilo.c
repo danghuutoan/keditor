32c1d9d14ab0f27be5a4af977285e1cd07525039
danghuutoan <danghuutoan@gmail.com>
2019-12-02 12:35:08 +0700

    add get window size function

--- kilo.c
+++ kilo.c
@@ -1,3 +1,4 @@
+#include <sys/ioctl.h>
 #include <errno.h>
 #include <ctype.h>
 #include <stdio.h>
@@ -57,6 +58,18 @@ char editorReadKey() {
   }
   return c;
 }
+
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    return -1;
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
 /*** input ***/
 void editorProcessKeypress() {
   char c = editorReadKey();
