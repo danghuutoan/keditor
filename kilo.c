207d819d33c12bc81b73e293a7ba241c6fe908cc
danghuutoan <danghuutoan@gmail.com>
2019-12-02 12:38:20 +0700

    draw the lines of tidles using the stored window size

--- kilo.c
+++ kilo.c
@@ -87,7 +87,7 @@ void editorProcessKeypress() {
 /*** output ***/
 void editorDrawRows() {
   int y;
-  for (y = 0; y < 24; y++) {
+  for (y = 0; y < E.screenrows; y++) {
     write(STDOUT_FILENO, "~\r\n", 3);
   }
 }
