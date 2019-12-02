c588d653a3dcfc5d38b204c488d26acd2785b135
danghuutoan <danghuutoan@gmail.com>
2019-12-02 20:17:20 +0700

    add enum for arrow keys

--- kilo.c
+++ kilo.c
@@ -11,6 +11,13 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define KILO_VERSION "0.0.1"
 
+enum editorKey {
+  ARROW_LEFT = 'a',
+  ARROW_RIGHT = 'd',
+  ARROW_UP = 'w',
+  ARROW_DOWN = 's'
+};
+
 struct editorConfig {
     int cx, cy;
     int screenrows;
@@ -67,10 +74,10 @@ char editorReadKey() {
     if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
     if (seq[0] == '[') {
         switch (seq[1]) {
-        case 'A': return 'w';
-        case 'B': return 's';
-        case 'C': return 'd';
-        case 'D': return 'a';
+        case 'A': return ARROW_UP;
+        case 'B': return ARROW_DOWN;
+        case 'C': return ARROW_RIGHT;
+        case 'D': return ARROW_LEFT;
         }
     }
         return '\x1b';
