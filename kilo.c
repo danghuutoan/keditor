cee42db99f712fd4f0cc8d81da5c13ae99cfe4de
danghuutoan <danghuutoan@gmail.com>
2019-12-02 12:01:45 +0700

    update a few more flags with descriptions

--- kilo.c
+++ kilo.c
@@ -14,10 +14,18 @@ void enableRawMode() {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
   struct termios raw = orig_termios;
-  // disable Ctrl-S and Ctrl-Q and Fix Ctrl-M
-  raw.c_iflag &= ~(ICRNL | IXON);
+  /* 
+   * disable Ctrl-S and Ctrl-Q and Fix Ctrl-M
+   * BRKINT : send a SIGINT signal to the program
+   * INPCK : enable parity checking
+  */
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   // turn off "\n" and "\r\n"
   raw.c_oflag &= ~(OPOST);
+    /*
+     * CS8 : set character size to 8 bits per byte
+     */
+  raw.c_cflag |= (CS8);
   // disable ECHO Canonical Ctrl-C Ctrl-Z and Ctr-V
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
