4a35f5d9a11718e7047dac25d0144df93c609870
danghuutoan <danghuutoan@gmail.com>
2019-12-02 20:10:22 +0700

    add welcome message

--- kilo.c
+++ kilo.c
@@ -9,6 +9,7 @@
 
 /*** defines ***/
 #define CTRL_KEY(k) ((k) & 0x1f)
+#define KILO_VERSION "0.0.1"
 
 struct editorConfig {
     int screenrows;
@@ -128,8 +129,24 @@ void editorProcessKeypress() {
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    abAppend(ab, "~", 1);
+    if (y == E.screenrows / 3) {
+        char welcome[80];
+        int welcomelen = snprintf(welcome, sizeof(welcome),
+        "Kilo editor -- version %s", KILO_VERSION);
+        if (welcomelen > E.screencols) welcomelen = E.screencols;
+        int padding = (E.screencols - welcomelen) / 2;
+        if (padding) {
+        abAppend(ab, "~", 1);
+        padding--;
+        }
+        while (padding--) abAppend(ab, " ", 1);
 
+        abAppend(ab, welcome, welcomelen);
+    } else {
+        abAppend(ab, "~", 1);
+    }
+
+    abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
       abAppend(ab, "\r\n", 2);
     }
@@ -143,11 +160,12 @@ void initEditor() {
 
 void editorRefreshScreen() {
     struct abuf ab = ABUF_INIT;
-    abAppend(&ab, "\x1b[2J", 4);
+    abAppend(&ab, "\x1b[?25l", 6);
     abAppend(&ab, "\x1b[H", 3);
     editorDrawRows(&ab);
-    
+
     abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[?25h", 6);
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
 
