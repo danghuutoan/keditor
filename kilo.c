3fe320541e94017af61d69fc3a07046b9357e7e5
danghuutoan <danghuutoan@gmail.com>
2019-12-05 07:59:26 +0700

    add horizontal scrolling

--- kilo.c
+++ kilo.c
@@ -35,6 +35,7 @@ typedef struct erow {
 struct editorConfig {
     int cx, cy;
     int rowoff;
+    int coloff;
     int screenrows;
     int screencols;
     struct termios orig_termios;
@@ -56,6 +57,12 @@ void editorScroll() {
   }
   if (E.cy >= E.rowoff + E.screenrows) {
     E.rowoff = E.cy - E.screenrows + 1;
+  }
+  if (E.cx < E.coloff) {
+    E.coloff = E.cx;
+  }
+  if (E.cx >= E.coloff + E.screencols) {
+    E.coloff = E.cx - E.screencols + 1;
   }
 }
 
@@ -218,14 +225,15 @@ int getWindowSize(int *rows, int *cols) {
 
 /*** input ***/
 void editorMoveCursor(int key) {
-  switch (key) {
+    erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+    switch (key) {
     case ARROW_LEFT:
         if (E.cx != 0) {
             E.cx--;
         }
         break;
     case ARROW_RIGHT:
-        if (E.cx != E.screencols - 1) {
+        if (row && E.cx < row->size) {
             E.cx++;
         }
         break;
@@ -302,9 +310,10 @@ void editorDrawRows(struct abuf *ab) {
             abAppend(ab, "~", 1);
         }
     } else {
-      int len = E.row[filerow].size;
-      if (len > E.screencols) len = E.screencols;
-        abAppend(ab, E.row[filerow].chars, len);
+        int len = E.row[filerow].size - E.coloff;
+        if (len < 0) len = 0;
+        if (len > E.screencols) len = E.screencols;
+        abAppend(ab, &E.row[filerow].chars[E.coloff], len);
     }
     abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
@@ -320,6 +329,7 @@ void initEditor() {
     E.numrows = 0;
     E.row = NULL;
     E.rowoff = 0;
+    E.coloff = 0;
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
@@ -331,8 +341,8 @@ void editorRefreshScreen() {
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
-    // snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
+                                            (E.cx - E.coloff) + 1);
     abAppend(&ab, buf, strlen(buf));
 
     abAppend(&ab, "\x1b[?25h", 6);
